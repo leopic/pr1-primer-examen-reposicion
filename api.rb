6cc4fa068d255d32392ebfadcf64821ba6959fdc
Leo Picado <leopic@gmail.com>
2016-03-26 01:47:01 -0500

    Agregando 50 transacciones.

--- api.rb
+++ api.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
+require 'json'
 
-get '/transacciones/ultimas' do
-    "ultimas!"
+def seeder
+  transacciones = []
+
+  (1..50).each do |i|
+    transacciones.push(
+        {
+            :id => i,
+            :monto => (i % 2 == 0) ? (i * -5) : (i * 10),
+            :descripcion => "Transaccion numero #{i}"
+        }
+    )
+  end
+
+  transacciones
 end
 
-get '/transacciones/id' do
-    "ID"
+get '/transaccion/todas' do
+  content_type :json
+  seeder.to_json
+end
+
+get '/transaccion/id/:id' do
+  content_type :json
+  movimiento = seeder.select { |mov|
+    mov[:id] == Integer(params['id'])
+  }
+  movimiento.to_json
 end
